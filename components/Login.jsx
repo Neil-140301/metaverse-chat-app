d973ae19a6277c7ad4c1de9e73efb482ed6c727d
Neil-140301 <alvares.1@iitj.ac.in>
2021-12-17 16:16:17 +0530

    first commit

---
+++ components/Login.jsx
@@ -0,0 +1,44 @@
+import Image from 'next/image';
+import { useMoralis } from 'react-moralis';
+
+const Login = () => {
+  const { authenticate } = useMoralis();
+
+  return (
+    <div className="bg-black relative ">
+      <div
+        className="flex flex-col absolute z-50 h-[70%] items-center justify-center w-full
+        space-y-6
+      "
+      >
+        {/* logo */}
+        <Image
+          src="/images/login-logo.jpg"
+          width="200"
+          height="200"
+          className="object-cover rounded-full "
+        />
+
+        {/* login button */}
+        <button
+          onClick={authenticate}
+          className="bg-yellow-500 rounded-lg p-5 font-bold animate-pulse "
+        >
+          Login to the METAVERSE
+        </button>
+      </div>
+
+      <div className="w-full h-screen ">
+        {/* login form */}
+        <Image
+          src="https://links.papareact.com/55n"
+          // src="/images/login-bg.jpg"
+          layout="fill"
+          objectFit="cover"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Login;
